8e4ff05779f49eb37de153f8ead83c5f3e5aa9e7
mefe-aydin <m.efeaydin@outlook.com>
2021-08-02 01:02:20 +0300

    project created

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    semi: true,
+    bracketSpacing: true,
+    trailingComma: "all",
+    arrowParens: "always",
+    tabWidth: 2,
+    printWidth: 100,
+    endOfLine: "lf",
+};
